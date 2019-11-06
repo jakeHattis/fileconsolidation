86163f655a93387629e7c2bceb91ad521b919ad1
jakeHattis <jacobhattis@gmail.com>
2019-11-06 13:41:39 +1100

    Removed -Remove-Item and fixed naming convention

--- pdfmerge.ps1
+++ pdfmerge.ps1
@@ -1,11 +1,9 @@
 forEach ($dir in get-childItem "test" -Attributes Directory -Recurse) {
 $parent = split-path -Path $dir 
-$parent = $parent.Replace("z:\test", "")
+$parent = $parent.Replace("Z:\test\", "")
 $parent = $parent.Replace("\", " ")
 $name = split-path -Path $dir -LeafBase
-$out = "z:\All Documents\"
+$out = "Z:\All Documents\"
 $out = $out + $parent + $name + ".pdf"
 Merge-Pdf -Path $dir -OutputPath $out -Force
-foreach ($file in get-childItem $dir -Attributes !Directory) {
-Remove-Item $file
-}}
+}
