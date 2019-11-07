966e1ed2578798212316f66e80d04360a7a0bba8
jakeHattis <jacobhattis@gmail.com>
2019-11-08 07:43:10 +1100

    Fixed bug in naming convention (Z:\test\)

--- pdfmerge.ps1
+++ pdfmerge.ps1
@@ -1,9 +1,9 @@
 forEach ($dir in get-childItem "test" -Attributes Directory -Recurse) {
 $parent = split-path -Path $dir 
-$parent = $parent.Replace("Z:\test\", "")
+$parent = $parent.Replace("Z:\test", "")
 $parent = $parent.Replace("\", " ")
 $name = split-path -Path $dir -LeafBase
-$out = "Z:\All Documents\"
+$out = "D:\Prop Photos\"
 $out = $out + $parent + $name + ".pdf"
 Merge-Pdf -Path $dir -OutputPath $out -Force
 }
