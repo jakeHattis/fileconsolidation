2e02f951e7773a1f971c52efaf790083f4f2f65a
jakeHattis <jacobhattis@gmail.com>
2019-11-19 13:14:35 +1100

    Changed cmdlets from split-path to convert-path

--- pdfmerge.ps1
+++ pdfmerge.ps1
@@ -1,9 +1,8 @@
 forEach ($dir in get-childItem "test" -Attributes Directory -Recurse) {
-$parent = split-path -Path $dir 
-$parent = $parent.Replace("Z:\test", "")
-$parent = $parent.Replace("\", " ")
-$name = split-path -Path $dir -LeafBase
-$out = "D:\Prop Photos\"
-$out = $out + $parent + $name + ".pdf"
-Merge-Pdf -Path $dir -OutputPath $out -Force
-}
+$path = convert-path $dir
+$path = $path.Replace("Z:\test\", "")
+$path = $path.Replace("\", " ")
+$path = $path.Replace(".", ",")
+$out = "Y:\Prop Photos\"
+$out = $out + $path + ".pdf"
+Merge-Pdf -Path $dir -OutputPath $out -Force}
